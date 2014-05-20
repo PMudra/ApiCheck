1447628ddced8e265ed899f6d03d128bb4b9dc32
PMudra <PMudra@users.noreply.github.com>
2014-05-20 14:03:00 +0200

    Removed failing tests from project.

--- ApiCheckTest/TestProvider/TestCaseSourceTest.cs
+++ ApiCheckTest/TestProvider/TestCaseSourceTest.cs
@@ -1,9 +1,8 @@
 using ApiCheckNUnit;
-using NUnit.Framework;
 
 namespace ApiCheckTest.TestProvider
 {
-  [TestFixture]
+  //[TestFixture]
   [ApiTest(@"TestProject\Version1\ApiCheckTestProject.dll", @"TestProject\Version2\ApiCheckTestProject.dll", Category = "ApiTest", IgnoreListPath = @"TestProvider\ignore.txt")]
   // Some of these tests must fail. Exclude ApiTest category from CI.
   public class TestCaseSourceTest : ApiTest
