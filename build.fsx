d66af41074c72df3595394bcf900e621f20ea5ac
PMudra <PMudra@users.noreply.github.com>
2014-05-21 16:59:50 +0200

    Build script modifications.

--- build.fsx
+++ build.fsx
@@ -10,14 +10,15 @@ let deployDir = @".\deploy\"
 let buildVersion = if isLocalBuild then "0" else buildVersion
 let version = "1.0." + buildVersion
 
-let description = "desc"
-let authors = ["authors"]
-let project = "project"
-let copyright = "(c)"
-let releaseNotes = "releaseNotes"
-let summary = "summary"
-let title = "title"
-let tags = "tags"
+let authors = ["ise Individuelle Software-Entwicklung GmbH"]
+let releaseNotes = "Initial Release."
+let tags = "TODO"
+
+let packages =
+    ["ApiCheck", "desc", "summary", []
+     "ApiCheck.Console", "desc", "summary", []
+     "ApiCheck.NUnit", "desc", "summary", ["ApiCheck", version
+                                           "NUnit", "2.6.3"]]
 
 Target "Clean" (fun _ ->
     CleanDirs [buildDir; deployDir]
@@ -31,57 +32,70 @@ Target "SetVersion" (fun _ ->
 
 Target "Compile" (fun _ ->
     !! @"**\ApiCheck.csproj"
-    ++ @"**\ApiCheckConsole.csproj"
-    ++ @"**\ApiCheckNUnit.csproj"
-        |> MSBuildRelease buildDir "Build"
-        |> Log "Compile-Output: "
+    ++ @"**\ApiCheck.Console.csproj"
+    ++ @"**\ApiCheck.NUnit.csproj"
+    |> MSBuildRelease buildDir "Build"
+    |> Log "Compile-Output: "
 )
 
 Target "CompileTest" (fun _ ->
-    !! @"**\ApiCheckTest.csproj"
+    !! @"**\ApiCheck.Test.csproj"
     |> MSBuildDebug buildDir "Build"
     |> Log "CompileTest-Output: "
 )
 
 Target "RunTest" (fun _ ->
-    !! (buildDir + @"*Test.dll")
+    !! (buildDir @@ @"*Test.dll")
     |> NUnit (fun p ->
         {p with
             DisableShadowCopy = true;
-            OutputFile = buildDir + @"TestResults.xml"
+            OutputFile = buildDir @@ @"TestResults.xml"
             ExcludeCategory = "ApiTest"})
 )
 
 Target "Zip" (fun _ ->
-    !! (buildDir + "**\*")
-    |> Zip buildDir (deployDir + "ApiCheck." + version + ".zip")
+    !! (buildDir @@ "**\*")
+    |> Zip buildDir (deployDir @@ "ApiCheck." + version + ".zip")
 )
 
 Target "NuGet" (fun _ ->
-    let packagingDir = @".\packaging\"
-    let libDir = packagingDir @@ "lib"
-    CleanDirs [packagingDir; libDir]
-
-    Copy libDir [(buildDir @@ "ApiCheck.dll")]
-    !!(buildDir + "*.txt")
-    |> Copy packagingDir
-
-    NuGet (fun p ->
-        {p with
-            WorkingDir = packagingDir
-            OutputPath = deployDir
-            Description = description
-            Publish = false
-            Version = version
-            Authors = authors
-            Project = project
-            Copyright = copyright
-            ReleaseNotes = releaseNotes
-            Summary = summary
-            Title = title
-            Tags = tags
-            })
-            "ApiCheck.nuspec"
+    for package, description, summary, dependencies in packages do
+        let packagingDir = @".\packaging\"
+        let libDir = packagingDir @@ "lib"
+        let toolDir = packagingDir @@ "tools"
+        CleanDirs [packagingDir; libDir; toolDir]
+        !! (buildDir @@ "*.txt") |> CopyFiles packagingDir
+        match package with
+        | "ApiCheck" ->
+            DeleteDir toolDir
+            CopyFile libDir (buildDir @@ "ApiCheck.dll")
+            CopyFile libDir (buildDir @@ "ApiCheck.XML")
+        | "ApiCheck.NUnit" ->
+            DeleteDir toolDir
+            CopyFile libDir (buildDir @@ "ApiCheck.NUnit.dll")
+            CopyFile libDir (buildDir @@ "ApiCheck.NUnit.XML")
+        | "ApiCheck.Console" ->
+            DeleteDir libDir
+            CopyFile toolDir (buildDir @@ "ApiCheck.dll")
+            CopyFile toolDir (buildDir @@ "ApiCheck.Console.exe")
+            CopyFile toolDir (buildDir @@ "ApiCheck.Console.exe.config")
+            CopyFile toolDir (buildDir @@ "CommandLine.dll")
+        | _ -> ()
+        NuGet (fun p ->
+            {p with
+                WorkingDir = packagingDir
+                OutputPath = deployDir
+                Publish = false
+                Description = description
+                Version = version
+                Authors = authors
+                Project = package
+                ReleaseNotes = releaseNotes
+                Summary = summary
+                Tags = tags
+                Dependencies = dependencies
+                })
+                "ApiCheck.nuspec"
 )
 
 Target "Default" DoNothing
