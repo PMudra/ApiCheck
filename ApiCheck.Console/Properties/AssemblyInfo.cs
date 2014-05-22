78c65f1a2727924b319e9b7eab3c2f2e87f54ba8
PMudra <PMudra@users.noreply.github.com>
2014-05-22 14:00:58 +0200

    Strong named assembly.

--- ApiCheck.Console/Properties/AssemblyInfo.cs
+++ ApiCheck.Console/Properties/AssemblyInfo.cs
@@ -22,4 +22,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("cfb26732-fc6d-40d1-a12d-8df07589482f")]
 
-[assembly: InternalsVisibleTo("ApiCheck.Test")]
+[assembly: InternalsVisibleTo("ApiCheck.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100635af544616a0dbfac252adb2e838384cf10d927f5af2c3e6198d849f95ce0defd3a248581454ff1c2b533a9ee9f86934ada35b809577bc7ca604405b7f239471938cf3a9dee77c9e1661c8bbdede4c605429446702962ebc6d0bf8ea72276bd004f31dfec132460bb2ffcd26a9c46e3916c302adbbd3a050394e6704dfd4f94")]
